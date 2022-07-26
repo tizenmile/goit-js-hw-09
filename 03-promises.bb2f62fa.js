7fc2fe197399a966aebc3b496ceb734e125163ff
tizenmile <tizen48@gmail.com>
2022-07-26 06:57:25 +0000

    Deploying to gh-pages from @ tizenmile/goit-js-hw-09@3178542c557f84dfb45752e391cbb7f3e12e39cf 🚀

---
+++ 03-promises.bb2f62fa.js
@@ -0,0 +1,2 @@
+document.getElementsByName("delay"),document.getElementsByName("step"),document.getElementsByName("amount");document.querySelector(".button").addEventListener("submit",(e=>{console.log(e.cancelable),e.preventDefault(),Math.random()}));
+//# sourceMappingURL=03-promises.bb2f62fa.js.map
---
+++ 03-promises.f5a87f2e.js
@@ -0,0 +1,2 @@
+!function(){document.getElementsByName("delay"),document.getElementsByName("step"),document.getElementsByName("amount");document.querySelector(".button").addEventListener("submit",(function(e){console.log(e.cancelable),e.preventDefault(),Math.random()}))}();
+//# sourceMappingURL=03-promises.f5a87f2e.js.map
