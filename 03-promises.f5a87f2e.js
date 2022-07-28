f80ddf1b3f32398dd60956f8f352573798725b8f
tizenmile <tizen48@gmail.com>
2022-07-28 08:36:41 +0000

    Deploying to gh-pages from @ tizenmile/goit-js-hw-09@06eada88033e28f0bb12150d28fea70411997690 🚀

--- 03-promises.f5a87f2e.js
+++
@@ -1,2 +0,0 @@
-!function(){document.getElementsByName("delay"),document.getElementsByName("step"),document.getElementsByName("amount");document.querySelector(".button").addEventListener("submit",(function(e){console.log(e.cancelable),e.preventDefault(),Math.random()}))}();
-//# sourceMappingURL=03-promises.f5a87f2e.js.map
